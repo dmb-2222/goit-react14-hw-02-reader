a3a6733385b619e56e05c968ed2462309815b7c5
dmb-2222 <dmb-2222@yandex.ru>
2019-11-02 14:00:26 +0200

    Updates

---
+++ precache-manifest.4616d54b1590f9f296240e4166ca8301.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a624180010c2f535059cda3f2a80a340",
+    "url": "/goit-react14-hw-02-reader/index.html"
+  },
+  {
+    "revision": "5333ceaae5d8b807633d",
+    "url": "/goit-react14-hw-02-reader/static/css/main.07c53a0d.chunk.css"
+  },
+  {
+    "revision": "9865f3f4f8cc4f06f79d",
+    "url": "/goit-react14-hw-02-reader/static/js/2.d9d75ffa.chunk.js"
+  },
+  {
+    "revision": "5333ceaae5d8b807633d",
+    "url": "/goit-react14-hw-02-reader/static/js/main.2b8e1070.chunk.js"
+  },
+  {
+    "revision": "8cec1c80a5708c7c3b0b",
+    "url": "/goit-react14-hw-02-reader/static/js/runtime-main.3d48a943.js"
+  }
+]);
